13e3fab4636971ecd3c7aa7d26fa1eae2619b3b8
HigoorCordeiroSantos <106189531+HigoorCordeiroSantos@users.noreply.github.com>
2022-10-18 16:49:33 -0300

    aula 18/10

---
+++ estudo.js
@@ -0,0 +1,123 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@HigoorCordeiroSantos 
+gabicassi
+/
+primeiroh_3t
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+primeiroh_3t/estudo.js /
+@gabicassi
+gabicassi aula 18/10
+Latest commit ba52b23 4 minutes ago
+ History
+ 1 contributor
+82 lines (72 sloc)  2.52 KB
+
+//alert('Oi, eu sou o alert!')
+//console.log('Oi, eu estou no console');
+//document.write('aula de PC');
+
+var tabuada = 8;
+
+function meChame(nome){
+    document.write("<h1>Tabuada do " + tabuada + "</h1>");
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br");
+    document.write("Feita por " + nome);
+    
+}
+
+function escreva(){
+    for(var i = 1; i < 11; i++){
+        document.write(tabuada + " x " + i + " = "+(tabuada*i)+"<br>");
+    }
+}
+
+function quadrado(){
+    for(var i = 2; i < 101; i++){
+        document.write("O Quadrado de " + i + " é " + (i*i)+ "<br>")
+    }
+}
+function total(){
+    let valor = document.getElementById("val").value;
+    let juros = document.getElementById("ju").value;
+    let mes = document.getElementById("mes").value;
+    let resultado = 0;
+   
+   if(!Number(valor)){
+    alert( "O juros deve ser um número.")   
+    return
+}
+if(!Number(mes)){
+    alert( "O mês deve ser um número.")   
+    return
+}
+if(!Number(juros)){
+    alert( "O valor deve ser um número.")   
+    return
+}
+
+
+   
+    for(let i = 1; i <= mes; i++){
+        resultado = valor * (1+(juros/100));
+        valor = resultado;
+    }
+    
+    document.write("O Resultado é " + resultado);
+}
+function soma(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let r = Number(n1) + Number(n2) + Number(n3);
+    document.getElementById("resultado").innerHTML = r;
+}
+function média(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let r = (Number(n1) + Number(n2) + Number(n3))/3;
+    document.getElementById("resultado").innerHTML = r;
+}
+function precisa(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let r = 180 - (Number(n1) + Number(n2) + Number(n3));
+    document.getElementById("resultado").innerHTML = r;
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
